a7ae6503aa527d77a832c9ed8c6b1fa9670325ef
Roger Vendrell <47077975+rogervendrell@users.noreply.github.com>
2025-03-11 11:15:02 +0100

    Create misc folder for comparisons

---
+++ misc/graphfilter.py
@@ -0,0 +1,131 @@
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+
+import utils.graphUtils.graphTools as graphTools
+
+zeroTolerance = 1e-9  # Values below this number are considered zero.
+infiniteNumber = 1e12  # infinity equals this number
+
+
+class GraphFilter(nn.Module):
+    """
+    GraphFilter Creates a (linear) layer that applies a graph filter
+
+    Initialization:
+
+        GraphFilter(in_features, out_features, filter_taps,
+                    edge_features=1, bias=True)
+
+        Inputs:
+            in_features (int): number of input features (each feature is a graph
+                signal)
+            out_features (int): number of output features (each feature is a
+                graph signal)
+            filter_taps (int): number of filter taps
+            edge_features (int): number of features over each edge
+            bias (bool): add bias vector (one bias per feature) after graph
+                filtering
+
+        Output:
+            torch.nn.Module for a graph filtering layer (also known as graph
+            convolutional layer).
+
+        Observation: Filter taps have shape
+            out_features x edge_features x filter_taps x in_features
+
+    Add graph shift operator:
+
+        GraphFilter.addGSO(GSO) Before applying the filter, we need to define
+        the GSO that we are going to use. This allows to change the GSO while
+        using the same filtering coefficients (as long as the number of edge
+        features is the same; but the number of nodes can change).
+
+        Inputs:
+            GSO (torch.tensor): graph shift operator; shape:
+                edge_features x number_nodes x number_nodes
+
+    Forward call:
+
+        y = GraphFilter(x)
+
+        Inputs:
+            x (torch.tensor): input data; shape:
+                batch_size x in_features x number_nodes
+
+        Outputs:
+            y (torch.tensor): output; shape:
+                batch_size x out_features x number_nodes
+    """
+
+    def __init__(self, G, F, K, E=1, bias=True):
+        # K: Number of filter taps
+        # GSOs will be added later.
+        # This combines both weight scalars and weight vectors.
+        # Bias will always be shared and scalar.
+
+        # Initialize parent
+        super().__init__()
+        # Save parameters:
+        self.G = G
+        self.F = F
+        self.K = K
+        self.E = E
+        self.S = None  # No GSO assigned yet
+        # Create parameters:
+        self.weight = nn.parameter.Parameter(torch.Tensor(F, E, K, G))
+        if bias:
+            self.bias = nn.parameter.Parameter(torch.Tensor(F, 1))
+        else:
+            self.register_parameter('bias', None)
+        # Initialize parameters
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # Taken from _ConvNd initialization of parameters:
+        stdv = 1. / math.sqrt(self.G * self.K)
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def addGSO(self, S):
+        # Every S has 3 dimensions.
+        assert len(S.shape) == 3
+        # S is of shape E x N x N
+        assert S.shape[0] == self.E
+        self.N = S.shape[1]
+        assert S.shape[2] == self.N
+        self.S = S
+
+    def forward(self, x):
+        # x is of shape: batchSize x dimInFeatures x numberNodesIn
+        B = x.shape[0]
+        F = x.shape[1]
+        Nin = x.shape[2]
+        # And now we add the zero padding
+        if Nin < self.N:
+            x = torch.cat((x,
+                           torch.zeros(B, F, self.N-Nin)
+                           .type(x.dtype).to(x.device)
+                           ), dim=2)
+        # Compute the filter output
+        u = LSIGF(self.weight, self.S, x, self.bias)
+        # So far, u is of shape batchSize x dimOutFeatures x numberNodes
+        # And we want to return a tensor of shape
+        # batchSize x dimOutFeatures x numberNodesIn
+        # since the nodes between numberNodesIn and numberNodes are not required
+        if Nin < self.N:
+            u = torch.index_select(u, 2, torch.arange(Nin).to(u.device))
+        return u
+
+    def extra_repr(self):
+        reprString = "in_features=%d, out_features=%d, " % (
+            self.G, self.F) + "filter_taps=%d, " % (
+            self.K) + "edge_features=%d, " % (self.E) +\
+            "bias=%s, " % (self.bias is not None)
+        if self.S is not None:
+            reprString += "GSO stored"
+        else:
+            reprString += "no GSO stored"
+        return reprString
